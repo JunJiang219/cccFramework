e8e2aa1bbc5f4e9f4f1ed4aee0b12f1ba173bb6a
jiangjun <916376983@qq.com>
2021-07-11 02:53:13 +0800

    初始化

---
+++ assets/example/ex_res/script/ExRes.ts
@@ -0,0 +1,57 @@
+
+import { _decorator, Component, SpriteFrame, Sprite, EventTouch, assetManager, resources } from 'cc';
+import { ResLeakChecker } from '../../../script/res/ResLeakChecker';
+import { resLoader } from '../../../script/res/ResLoader';
+const { ccclass, property } = _decorator;
+
+@ccclass('ExRes')
+export class ExRes extends Component {
+
+    @property(Sprite)
+    spr: Sprite = null!;
+
+    start() {
+        ResLeakChecker.getInstance().startCheck();
+    }
+
+    public loadSprite(path: string) {
+        // 官方接口
+        // resources.load(path, SpriteFrame, (err, asset: SpriteFrame) => {
+        //     if (err) return;
+        //     this.spr.spriteFrame = asset;
+        //     asset.addRef();
+        // })
+
+        // 框架接口
+        resLoader.load(path, SpriteFrame, (err, asset: SpriteFrame) => {
+            if (err) return;
+            this.spr.spriteFrame = asset;
+            ResLeakChecker.getInstance().traceAsset(asset);
+            asset.addRef();
+        })
+    }
+
+    public clickBtn_loadSprite(event: EventTouch) {
+        this.loadSprite('texture/test/spriteFrame');
+    }
+
+    public clickBtn_decSprite(event: EventTouch) {
+        this.spr.spriteFrame?.decRef()
+    }
+
+    public clickBtn_releaseSprite(event: EventTouch) {
+        assetManager.releaseAsset(this.spr.spriteFrame!);
+    }
+
+    public clickBtn_releaseUnusedAssets(event: EventTouch) {
+        resources.releaseUnusedAssets();
+    }
+
+    public dumpAsset() {
+        console.log(assetManager.assets);
+    }
+
+    public dumpTrace() {
+        ResLeakChecker.getInstance().dump();
+    }
+}
---
+++ assets/script/res/ResLeakChecker.ts
@@ -0,0 +1,159 @@
+/**
+ * 资源泄露检查类，可以用于跟踪资源的引用情况
+ * 
+ * 2021-1-31 by 宝爷
+ */
+
+import { Asset, isValid } from "cc";
+import { ResUtil } from "./ResUtil";
+
+export type FilterCallback = (asset: Asset) => boolean;
+
+declare module "cc" {
+    interface Asset {
+        traceMap?: Map<string, number>;
+        resetTrace?: () => void;
+    }
+}
+
+export class ResLeakChecker {
+    private static _instance: ResLeakChecker | null = null;
+    private constructor() {}
+    public static getInstance(): ResLeakChecker {
+        if (!this._instance) this._instance = new ResLeakChecker();
+        return this._instance;
+    }
+
+    public resFilter: FilterCallback | null = null;    // 资源过滤回调(需要追踪的资源返回 true)
+    private _checking: boolean = false;
+    private traceAssets: Set<Asset> = new Set<Asset>();
+
+    /**
+     * 检查该资源是否符合过滤条件
+     * @param url 
+     */
+    public checkFilter(asset: Asset): boolean {
+        if (!this._checking) {
+            return false;
+        }
+        if (this.resFilter) {
+            return this.resFilter(asset);
+        }
+        return true;
+    }
+
+    /**
+     * 通过uuid过滤资源
+     * @param idList uuid列表
+     */
+    public filterResByUUID(idList?: string[]) {
+        if (!idList || idList.length <= 0) this.resFilter = null;
+
+        let filterUUID = idList;
+        this.resFilter = function(asset: Asset): boolean {
+            if (asset && filterUUID?.indexOf(asset._uuid) != -1) return true;
+            return false;
+        }
+    }
+
+    /**
+     * 对资源进行引用的跟踪
+     * @param asset 
+     */
+    public traceAsset(asset: Asset) {
+        if (!asset || !this.checkFilter(asset)) {
+            return;
+        }
+        if (!this.traceAssets.has(asset)) {
+            // asset.addRef();
+            this.traceAssets.add(asset);
+            this.extendAsset(asset);
+        }
+    }
+
+    /**
+     * 扩展asset，使其支持引用计数追踪
+     * @param asset 
+     */
+    public extendAsset(asset: Asset) {
+        let addRefFunc = asset.addRef;
+        let decRefFunc = asset.decRef;
+        let traceMap = new Map<string, number>();
+        asset.traceMap = traceMap;
+        asset.addRef = function (...args: any): Asset {
+            let stack = ResUtil.getCallStack(1);
+            let cnt = traceMap.has(stack) ? traceMap.get(stack)! + 1 : 1;
+            traceMap.set(stack, cnt);
+            return addRefFunc.apply(asset, args);
+        }
+        asset.decRef = function (...args: any): Asset {
+            let stack = ResUtil.getCallStack(1);
+            let cnt = traceMap.has(stack) ? traceMap.get(stack)! + 1 : 1;
+            traceMap.set(stack, cnt);
+            return decRefFunc.apply(asset, args);
+        }
+        asset.resetTrace = () => {
+            asset.addRef = addRefFunc;
+            asset.decRef = decRefFunc;
+            delete asset.traceMap;
+        }
+    }
+
+    /**
+     * 还原asset，使其恢复默认的引用计数功能
+     * @param asset 
+     */
+    public resetAsset(asset: Asset) {
+        if (asset.resetTrace) {
+            asset.resetTrace();
+        }
+    }
+
+    public unTraceAsset(asset: Asset) {
+        if (this.traceAssets.has(asset)) {
+            this.resetAsset(asset);
+            // asset.decRef();
+            this.traceAssets.delete(asset);
+        }
+    }
+
+    public startCheck() { this._checking = true; }
+    public stopCheck() { this._checking = false; }
+    public getTraceAssets(): Set<Asset> { return this.traceAssets; }
+
+    public reset() {
+        this.traceAssets.forEach(element => {
+            this.resetAsset(element);
+            // element.decRef();
+        });
+        this.traceAssets.clear();
+    }
+
+    public dump() {
+        // 删除失效资源
+        let inValidAssets: Asset[] = [];
+        this.traceAssets.forEach(element => {
+            if (!isValid(element)) inValidAssets.push(element);
+        });
+        for (let i = 0; i < inValidAssets.length; ++i) this.traceAssets.delete(inValidAssets[i]);
+        inValidAssets = null!;
+
+        this.traceAssets.forEach(element => {
+            let traceMap: Map<string, number> | undefined = element.traceMap;
+            if (traceMap) {
+                console.log('----------------------------------------------');
+                let unRef = true;
+                traceMap.forEach((key, value) => {
+                    unRef = false;
+                    console.log(`${key} : ${value} `);
+                });
+                if (unRef) {
+                    console.log(`资源未调用引用计数, uuid = ${element._uuid}`);
+                    console.log(element);
+                }
+            }
+        })
+    }
+}
+
+window['resLeakChecker'] = ResLeakChecker.getInstance();
