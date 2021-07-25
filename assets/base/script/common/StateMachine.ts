6a0c91988875dd34279e0161a75e729f9096fea5
jiangjun <916376983@qq.com>
2021-07-26 01:58:55 +0800

    commit

--- assets/base/script/common/StateMachine.ts
+++ assets/base/script/common/StateMachine.ts
@@ -32,12 +32,20 @@ export interface IFsmInitObj {
     methods?: { [lifecycle: string]: Function }
 }
 
-interface IFsmLifecycle {
-    onBeforeTransition?: { [transitName: string]: string },
-    onLeaveState?: { [stateName: string]: string },
-    onTransition?: { [transitName: string]: string },
-    onEnterState?: { [stateName: string]: string },
-    onAfterTransition?: { [transitName: string]: string },
+export interface IFsmLifecycleEvent {
+    onBefore?: { [key: string]: string },
+    onAfter?:  { [key: string]: string },
+    onEnter?:  { [key: string]: string },
+    onLeave?:  { [key: string]: string },
+    on?:       { [key: string]: string },
+}
+
+export interface IFsmTransitArgs {
+    transition?: string,
+    from?: string,
+    to?: string,
+    fsm?: StateMachine,
+    event?: string
 }
 
 function camelize(label: string) {
@@ -69,9 +77,12 @@ export class StateMachine {
     private _pending: boolean = false;      // 状态转换中，挂起
     private _states: string[] = [];         // 全部状态名
     private _transitions: string[] = [];    // 全部转换名
-    private _lifecycle = null;
-    private _map: { [stateName: string]: { [transitName: string]: IFsmTransition } } = {};
+    private _lifecycle: IFsmLifecycleEvent = {};
+    private observers: any[] = [];
+    private _map: { [state: string]: { [transition: string]: IFsmTransition } } = {};
     public constructor(param: IFsmInitObj) {
+        this.observers = [this];
+        this._configureLifecycle();
         this._configureInitTransition(param.init!);
         this.configureTransitions(param.transitions!);
     }
@@ -84,15 +95,31 @@ export class StateMachine {
     private _addState(name: string) {
         if (!this._map[name]) {
             this._states.push(name);
+            this._addStateLifecycleNames(name);
             this._map[name] = {};
         }
+    }
+
+    // 添加生命周期名 - 状态相关
+    private _addStateLifecycleNames(name: string) {
+        this._lifecycle.onEnter![name] = camelize_prepended('onEnter', name);
+        this._lifecycle.onLeave![name] = camelize_prepended('onLeave', name);
+        this._lifecycle.on![name]      = camelize_prepended('on',      name);
     }
 
     // 添加转换
     private _addTransition(name: string) {
         if (this._transitions.indexOf(name) < 0) {
             this._transitions.push(name);
+            this._addTransitionLifecycleNames(name);
         }
+    }
+
+    // 添加生命周期名 - 转换相关
+    private _addTransitionLifecycleNames(name: string) {
+        this._lifecycle.onBefore![name] = camelize_prepended('onBefore', name);
+        this._lifecycle.onAfter![name]  = camelize_prepended('onAfter',  name);
+        this._lifecycle.on![name]       = camelize_prepended('on',       name);
     }
 
     // 配置单条转换信息
@@ -105,6 +132,17 @@ export class StateMachine {
         this._addTransition(name);
         this._map[from][name] = transition;
         return transition;
+    }
+
+    // 配置默认生命周期名
+    private _configureLifecycle() {
+        this._lifecycle = {
+          onBefore: { transition: 'onBeforeTransition' },
+          onAfter:  { transition: 'onAfterTransition'  },
+          onEnter:  { state:      'onEnterState'       },
+          onLeave:  { state:      'onLeaveState'       },
+          on:       { transition: 'onTransition'       }
+        };
     }
 
     // 配置初始化转换信息
@@ -132,13 +170,73 @@ export class StateMachine {
         }
     }
 
+    private _observe(args) {
+        if (args.length === 2) {
+          var observer = {};
+          observer[args[0]] = args[1];
+          this.observers.push(observer);
+        }
+        else {
+          this.observers.push(args[0]);
+        }
+    }
+
+    private _observersForEvent(event: string) { // TODO: this could be cached
+        var n = 0, max = this.observers.length, observer, result = [];
+        for( ; n < max ; n++) {
+          observer = this.observers[n];
+          if (observer[event])
+            result.push(observer);
+        }
+        return [ event, result, true ]
+    }
+
+    // 观察事件
+    private _observeEvents(events: string[], args: IFsmTransitArgs[], previousEvent?: string, previousResult?: boolean) {
+        if (events.length === 0) {
+          return this._endTransit(previousResult === undefined ? true : previousResult);
+        }
+    
+        var event     = events[0][0],
+            observers = events[0][1],
+            // pluggable = events[0][2];
+    
+        args[0].event = event;
+        // if (event && pluggable && event !== previousEvent)
+        //   plugin.hook(this, 'lifecycle', args);
+    
+        if (observers.length === 0) {
+          events.shift();
+          return this._observeEvents(events, args, event, previousResult);
+        }
+        else {
+          var observer = observers.shift(),
+              result = observer[event].apply(observer, args);
+          if (result && typeof result.then === 'function') {
+            return result.then(this._observeEvents.bind(this, events, args, event))
+                         .catch(this._failTransit.bind(this))
+          }
+          else if (result === false) {
+            return this._endTransit(false);
+          }
+          else {
+            return this._observeEvents(events, args, event, result);
+          }
+        }
+    }
+
+    private _beginTransit()          { this._pending = true;                 }
+    private _endTransit(result: boolean)    { this._pending = false; return result; }
+    private _failTransit(result: any)    { this._pending = false; throw result;  }
+    private _doTransit(lifecycle) { this._state = lifecycle.to; }
+
     // 无效转换
-    private _onInvalidTransition(transitName: string, from: string, to: string) {
-        console.error("transition is invalid in current state", transitName, from, to, this._state);
+    private _onInvalidTransition(transition: string, from: string, to: string) {
+        console.error("transition is invalid in current state", transition, from, to, this._state);
     }
     
     // 转换进行中
-    private _onPendingTransition(transitName: string, from: string, to: string) {
-        console.warn("transition is invalid while previous transition is still in progress", transitName, from, to, this._state);
+    private _onPendingTransition(transition: string, from: string, to: string) {
+        console.warn("transition is invalid while previous transition is still in progress", transition, from, to, this._state);
     }
 }
