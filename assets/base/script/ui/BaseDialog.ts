566569bc65920dffc4d18cdab0b0424dc28218ea
jiangjun <916376983@qq.com>
2021-09-12 16:47:59 +0800

    commit

--- assets/base/script/ui/BaseDialog.ts
+++ assets/base/script/ui/BaseDialog.ts
@@ -15,6 +15,8 @@ export class BaseDialog extends ResKeeper {
     // 确认、取消回调
     private _okCb: DialogCallback | null = null;
     private _noCb: DialogCallback | null = null;
+    // 可变参数
+    private _uiArgs: any[] = [];
 
     // 界面索引
     private _uiIndex: number = 0;
@@ -31,9 +33,17 @@ export class BaseDialog extends ResKeeper {
         this._uiIndex = ++BaseDialog._uiCnt;
     }
 
+    /**
+     * 当界面被打开时回调，每次调用Open时回调
+     * @param content 提示内容
+     * @param okCallback 确认回调
+     * @param noCallback 取消回调
+     * @param args 可变参数
+     */
     public onOpen(content: string, okCallback?: DialogCallback, noCallback?: DialogCallback, ...args: any[]) {
         if (okCallback) this._okCb = okCallback;
         if (noCallback) this._noCb = noCallback;
+        this._uiArgs = args;
     }
 
     /**
@@ -42,13 +52,30 @@ export class BaseDialog extends ResKeeper {
     public onOpenAniOver(): void {
     }
 
-    /**
-     * 当界面被关闭时回调，每次调用Close时回调
-     * 返回值会传递给下一个界面
-     */
-    public onClose(): any {
-
+    // 确认按钮响应
+    public onOKTouch() {
+        if (this._okCb) {
+            let callback = this._okCb.callback;
+            let target = this._okCb.target;
+            if (target) {
+                callback.call(target, ...this._uiArgs);
+            } else {
+                callback(...this._uiArgs);
+            }
+        }
     }
 
+    // 取消按钮响应
+    public onNOTouch() {
+        if (this._noCb) {
+            let callback = this._noCb.callback;
+            let target = this._noCb.target;
+            if (target) {
+                callback.call(target, ...this._uiArgs);
+            } else {
+                callback(...this._uiArgs);
+            }
+        }
+    }
     /********************** UI的回调 ***********************/
 }
